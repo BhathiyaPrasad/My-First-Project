50f639aada2c910cb5969e42bc6418e1d10634d2
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2023-04-21 01:41:21 +0530

    creating base java programe testing variables

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        byte myAge = 21;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible;
+      System.out.println();
+    }
+}
