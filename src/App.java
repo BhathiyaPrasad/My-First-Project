b253dd35876e61377f9aa12cafcd4bde1ab7a7bb
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2023-04-25 23:47:53 +0530

    try new features

--- src/App.java
+++ src/App.java
@@ -1,10 +1,8 @@
+import java.util.Date;
 public class App {
-    public static void main(String[] args) throws Exception {
-        byte myAge = 21;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible;
-      System.out.println();
+    public static void main(String[] args) {
+        
+        Date now = new Date();
+        System.out.println(now);
     }
 }
