7833ebe6792df9f5407ae0d8d4ab0b811c07642f
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2023-04-29 00:22:09 +0530

    adding arrays values

---
+++ src/arrays.java
@@ -0,0 +1,12 @@
+
+    import java.util.Arrays;
+
+public class arrays {
+    public static void main(String[] args) {
+        int[] numbers = {5, 1, 3, 4, 6};
+        Arrays.sort(numbers);
+      
+        System.out.println(Arrays.toString(numbers));
+    }
+}
+
