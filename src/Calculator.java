8e3e97a4cce398a00982c6370dbadf5972ff583a
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2023-05-03 21:56:31 +0530

    simple java calculator for calculate monthly payments for a Loan

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,25 +3,20 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args) {
-        Double rate = (Double) 3.76;
-        Double period  = (Double) 3.6;
-        Scanner scanner = new Scanner(System.in);
-        Double annual =  scanner.nextDouble();
-        System.out.println(annual);
-        scanner.close();
-        Double mortgage = (Double) annual / (Double)rate + (Double) period;
-//         Scanner scanner = new Scanner(System.in);
-// Double value =  scanner.nextDouble();
-// scanner.close();
+  public static void main(String[] args) {
+    Double rate = (Double) 3.76;
+      Double years = (Double) 3.6;
+    System.out.print("Captial: ");
+    Scanner scanner = new Scanner(System.in);
+    Double principal = scanner.nextDouble();
 
+    scanner.close();
+    Double mortgage = (Double) principal / (Double) rate *( 1 + (Double) rate )*years / (1 + (Double) rate) *years - 1;
 
+    NumberFormat currency = NumberFormat.getCurrencyInstance();
+    String result = currency.format(mortgage);
 
-NumberFormat currency = NumberFormat.getCurrencyInstance(); 
-  String result = currency.format(mortgage) ;
-  
-  System.out.println(result); 
+    System.out.println("Monthly Payment= "+result);
 
-        
-    }
+  }
 }
