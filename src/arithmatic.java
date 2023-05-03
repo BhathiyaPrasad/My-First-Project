7a971affcb83cfde1253ae639029320be12e89e5
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2023-05-04 00:41:33 +0530

    edited

--- src/arithmatic.java
+++ src/arithmatic.java
@@ -3,7 +3,7 @@ public class arithmatic {
     public static void main(String[] args) {
         String x = "1";
         int y =  Integer.parseInt(x);
-        System.out.println(x);
+        System.out.println(y);
 
     }
 }
