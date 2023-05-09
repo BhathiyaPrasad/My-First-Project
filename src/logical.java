0af0210a6db37c6aa6668ebb8185c549607771b1
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2023-05-10 00:08:45 +0530

    boolean method for get know about the temparature is warm or cold

--- src/logical.java
+++ src/logical.java
@@ -1,5 +1,7 @@
 public class logical {
-    public static void  (String[] args){
-
+    public static void main (String[] args){
+       int temparature = 22;
+       boolean isWarm = temparature > 20 && temparature < 30;
+       System.out.println(isWarm);
     }
 }
